d811ee896d5f3057bfef6363191d3758c0f6a6fc
alain-kubwayo <ahkubwayo@gmail.com>
2023-03-03 15:20:19 +0200

    feat: add wrapper

---
+++ src/layout/Wrapper.jsx
@@ -0,0 +1,7 @@
+const Wrapper = ({ children }) => {
+    return ( 
+        <div className="bg-gray-100 p-4 md:p-8 lg:p-16 max-w-6xl mx-auto">{ children }</div>
+    );
+}
+ 
+export default Wrapper;
