cbebaac880af639888a1d4b57cc8d4cebb006895
alain-kubwayo <ahkubwayo@gmail.com>
2023-03-06 11:53:18 +0200

    feat: add skeleton of Home component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
     return ( 
-        <div className="text-secondary-100">Home Component</div>
+        <div className="text-secondary-100 text-5xl font-extrabold -tracking-[4px]">Home Component</div>
     );
 }
  
