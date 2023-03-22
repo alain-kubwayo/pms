5af2bcb5e118d9cdae93a79344dc701d6c73aeb0
alain-kubwayo <ahkubwayo@gmail.com>
2023-03-22 21:23:10 +0200

    feat:adjust wrapper

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
     <BrowserRouter>
       <Wrapper>
         <Routes>
-        
+        <Route path="/" element={<div className="text-red-700">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste eius sequi accusantium doloribus laborum fuga placeat maiores, vel, illum, blanditiis eos voluptate rerum quia! Doloremque at explicabo facere laborum. Reiciendis.</div>} />
         </Routes>
       </Wrapper>
     </BrowserRouter>
