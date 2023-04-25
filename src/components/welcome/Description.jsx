8b3b8870bf39ed30d62317ff1bec9a97aca6a62e
alain-kubwayo <ahkubwayo@gmail.com>
2023-04-25 16:11:11 +0200

    feat: update welcome page description

--- src/components/welcome/Description.jsx
+++ src/components/welcome/Description.jsx
@@ -4,12 +4,10 @@ const Description = () => {
             <h1 className="mb-4 text-4xl font-extrabold text-center text-tertiary lg:text-left">PMS</h1>
             <div className="space-y-4 text-lg text-tertiary lg:space-y-7">
                 <p>Managing projects has never been easy</p>
-                <p>With PMS, you can manage either a single or multiple projects in a single place.</p>
-                <p>With PMS, you can:</p>
+                <p>With PMS, you can ship projects in time by efficiently managing one or more projects from a central place through:</p>
                 <ul className="pl-4">
-                    <li>Track individual progress</li>
-                    <li>Track teams' progress</li>
-                    <li>And ship projects in time!</li>
+                    <li>Tracking individual progress</li>
+                    <li>Tracking teams' progress</li>
                 </ul>
             </div>
         </div>
