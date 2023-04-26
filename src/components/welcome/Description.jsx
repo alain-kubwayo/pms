8a2ca1c990b4f992755fb3840caa63575a3bc0f3
alain-kubwayo <ahkubwayo@gmail.com>
2023-04-26 17:52:32 +0200

    fix: rename class to className in Description component

--- src/components/welcome/Description.jsx
+++ src/components/welcome/Description.jsx
@@ -1,6 +1,6 @@
 const Description = () => {
     return ( 
-        <div class="text-center lg:text-left">
+        <div className="text-center lg:text-left">
             <h1 className="mb-4 text-4xl font-extrabold text-center text-tertiary lg:text-left">PMS</h1>
             <div className="space-y-4 text-lg text-tertiary lg:space-y-7">
                 <p>Managing projects has never been easy</p>
