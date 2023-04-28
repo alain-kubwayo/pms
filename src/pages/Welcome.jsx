2bf2122ddfe15bfa7eaa27a6ca4a3171dd6fce86
alain-kubwayo <ahkubwayo@gmail.com>
2023-04-28 17:42:29 +0200

    feat: add Particles component for welcome page animation

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -5,6 +5,7 @@ import RegisterForm from "../components/auth/RegisterForm";
 import DefaultButton from "../components/common/DefaultButton";
 import Heading from "../components/common/Heading";
 import Description from "../components/welcome/Description";
+import Particle from "../components/welcome/Particle";
 
 const Welcome = () => {
     const [activeTab, setActiveTab] = useState("login");
@@ -13,48 +14,51 @@ const Welcome = () => {
     const handleRegisterButtonClick = () => setActiveTab("register");
     
     return ( 
-        <div className="grid items-center w-full grid-cols-1 py-10 mx-auto lg:items-start lg:pt-40 lg:h-screen lg:py-0 md:w-4/5 gap-y-10 lg:grid-cols-2 md:gap-x-20">
-            <Description />
-            <div>
-                <Heading
-                    description="Let's get you started!"
-                    customClasses="text-center lg:text-left mb-4 text-2xl font-bold uppercase text-tertiary font-secondary border-t lg:border-none border-tertiary pt-4 lg:pt-0" 
-                />
-                <div className="flex flex-col items-center pb-4 border-b border-gray-700 gap-y-4 lg:flex-row gap-x-2 xl:gap-x-6">
-                    <span className="text-gray-400">Continue with: </span>
-                    <div className="space-x-4">
-                        <DefaultButton 
-                            description="Google" 
-                            customClasses="hover:text-black hover:bg-tertiary duration-500 rounded-md border border-tertiary"
-                        />
-                        <DefaultButton 
-                            description="LinkedIn" 
-                            customClasses="hover:text-black hover:bg-tertiary duration-500 rounded-md border border-tertiary"
-                        />
+        <>
+            <Particle />
+            <div className="z-40 grid items-center w-full grid-cols-1 py-10 mx-auto text-white lg:items-start lg:pt-40 lg:h-screen lg:py-0 md:w-4/5 gap-y-10 lg:grid-cols-2 md:gap-x-20">
+                <Description />
+                <div>
+                    <Heading
+                        description="Let's get you started!"
+                        customClasses="text-center lg:text-left mb-4 text-2xl font-bold uppercase text-tertiary font-secondary border-t lg:border-none border-tertiary pt-4 lg:pt-0" 
+                    />
+                    <div className="flex flex-col items-center pb-4 border-b border-gray-700 gap-y-4 lg:flex-row gap-x-2 xl:gap-x-6">
+                        <span className="text-gray-400">Continue with: </span>
+                        <div className="space-x-4">
+                            <DefaultButton 
+                                description="Google" 
+                                customClasses="hover:text-black hover:bg-tertiary duration-500 rounded-md border border-tertiary"
+                            />
+                            <DefaultButton 
+                                description="LinkedIn" 
+                                customClasses="hover:text-black hover:bg-tertiary duration-500 rounded-md border border-tertiary"
+                            />
+                        </div>
+                    </div>
+                    <div className="mt-8">
+                        <div>
+                            <button 
+                                className={`px-2 py-2 text-tertiary w-1/2 rounded-l-md border border-tertiary ${activeTab === 'login' ? 'active' : ''}`}
+                                onClick={handleLoginButtonClick}
+                            >
+                                Login
+                            </button>
+                            <button 
+                                className={`px-2 py-2 text-tertiary w-1/2 rounded-r-md border border-tertiary ${activeTab === 'register' ? 'active' : ''}`}
+                                onClick={handleRegisterButtonClick}
+                            >
+                                Register 
+                            </button>
+                        </div>
+                        <div>
+                            {activeTab === "login" ? <LoginForm /> : <RegisterForm />}
+                        </div>
                     </div>
                 </div>
-                <div className="mt-8">
-                    <div>
-                        <button 
-                            className={`px-2 py-2 text-tertiary w-1/2 rounded-l-md border border-tertiary ${activeTab === 'login' ? 'active' : ''}`}
-                            onClick={handleLoginButtonClick}
-                        >
-                            Login
-                        </button>
-                        <button 
-                            className={`px-2 py-2 text-tertiary w-1/2 rounded-r-md border border-tertiary ${activeTab === 'register' ? 'active' : ''}`}
-                            onClick={handleRegisterButtonClick}
-                        >
-                            Register 
-                        </button>
-                    </div>
-                    <div>
-                        {activeTab === "login" ? <LoginForm /> : <RegisterForm />}
-                    </div>
-                </div>
+                
             </div>
-            
-        </div>
+        </>
     );
 }
  
